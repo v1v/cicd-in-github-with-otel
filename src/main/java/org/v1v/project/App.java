b44f1dcfd20d671a6b5da83aac3563666242c87c
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-07-09 18:16:32 +0100

    feat: create maven java project

---
+++ src/main/java/org/v1v/project/App.java
@@ -0,0 +1,13 @@
+package org.v1v.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
